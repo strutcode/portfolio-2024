bc754ed213b48b907397b85ecffb61e83b06b372
Tristan Shelton <tristan.m.shelton@gmail.com>
2024-11-06 21:24:17 -0800

    Convert particles to an instanced array

--- src/webgl/scenes/HomeScene.ts
+++ src/webgl/scenes/HomeScene.ts
@@ -1,7 +1,10 @@
 import Scene from '../Scene'
 
 import {
+  addExtensionsToContext,
+  createBufferInfoFromArrays,
   createProgramInfo,
+  createVertexArrayInfo,
   drawBufferInfo,
   m4,
   primitives,
@@ -13,95 +16,81 @@ import {
 import vs from './particle.vs.glsl?raw'
 import fs from './particle.fs.glsl?raw'
 
-/** A simple 3D particle */
-export class Particle {
-  protected static programInfo?: ReturnType<typeof createProgramInfo>
-
-  public static dispose() {
-    Particle.programInfo = undefined
-  }
-
-  public worldTransform = m4.identity()
-  protected bufferInfo: ReturnType<typeof primitives.createXYQuadBufferInfo>
-
-  public constructor(gl: WebGLRenderingContext) {
-    // Create the shader program if it doesn't exist
-    if (!Particle.programInfo) {
-      Particle.programInfo = createProgramInfo(gl, [vs, fs])
-    }
-
-    // Create a buffer for the geometry
-    this.bufferInfo = primitives.createXYQuadBufferInfo(gl, 0.02)
-  }
-
-  public render(scene: Scene) {
-    if (!Particle.programInfo) return
-
-    const uniforms = {
-      worldView: m4.multiply(scene.camera.view, this.worldTransform),
-      worldViewProjection: m4.multiply(scene.camera.viewProjection, this.worldTransform),
-    }
-
-    const gl = scene.getContext()
-
-    // Set the shader
-    gl.useProgram(Particle.programInfo.program)
-
-    // Prepare the geometry
-    setBuffersAndAttributes(gl, Particle.programInfo, this.bufferInfo)
-
-    // Set the shader data
-    setUniforms(Particle.programInfo, uniforms)
-
-    // Finally draw
-    drawBufferInfo(gl, this.bufferInfo)
-  }
-}
-
 /** A fancy sparkling shimmery effect for the home page */
 export default class HomeScene extends Scene {
-  private particles: Particle[] = []
+  private particles = 10000
+  private data: Record<any, any> = {}
 
   public constructor(canvas: HTMLCanvasElement) {
     super(canvas)
+
+    addExtensionsToContext(this.getContext())
 
     this.createParticles()
   }
 
   public dispose() {
     super.dispose()
-
-    this.particles = []
-    Particle.dispose()
   }
 
   public createParticles() {
     const gl = this.getContext()
+
+    const numInstances = this.particles
+    const instanceWorlds = new Float32Array(numInstances * 16)
+    const randfRange = (x, y) => x + Math.random() * (y - x)
     const range = 20
 
-    for (let i = 0; i < 10000; i++) {
-      const particle = new Particle(gl)
-      const x = Math.random() * range - range * 0.5
-      const y = Math.random() * range * 0.5 - range * 0.25
-
-      particle.worldTransform = m4.rotateX(
-        particle.worldTransform,
-        Math.random() * Math.PI * 2,
+    for (let i = 0; i < numInstances; ++i) {
+      const mat = new Float32Array(instanceWorlds.buffer, i * 16 * 4, 16)
+      m4.translation(
+        [randfRange(-range, range), randfRange(-range, range), randfRange(0, range)],
+        mat,
       )
-      particle.worldTransform = m4.translate(particle.worldTransform, [x, y, 0])
-
-      this.particles.push(particle)
+      m4.rotateZ(mat, randfRange(0, Math.PI * 2), mat)
+      m4.rotateX(mat, randfRange(0, Math.PI * 2), mat)
     }
+
+    const arrays = primitives.createCubeVertices(0.1)
+    Object.assign(arrays, {
+      instanceWorld: {
+        numComponents: 16,
+        data: instanceWorlds,
+        divisor: 1,
+      },
+    })
+
+    const programInfo = createProgramInfo(gl, [vs, fs])
+    const bufferInfo = createBufferInfoFromArrays(gl, arrays)
+    const vertexArrayInfo = createVertexArrayInfo(gl, programInfo, bufferInfo)
+
+    this.data = {
+      programInfo,
+      bufferInfo,
+      vertexArrayInfo,
+    }
+
+    console.log(vertexArrayInfo)
   }
 
   public animate(deltaSeconds: number) {
-    for (const particle of this.particles) {
-      particle.worldTransform = m4.rotateX(
-        particle.worldTransform,
-        deltaSeconds * 3,
-      )
-
-      particle.render(this)
+    const gl = this.getContext()
+    const { programInfo, vertexArrayInfo } = this.data
+    const uniforms = {
+      view: this.camera.view,
+      viewProjection: this.camera.viewProjection,
     }
+
+    gl.useProgram(programInfo.program)
+    setBuffersAndAttributes(gl, programInfo, vertexArrayInfo)
+    setUniforms(programInfo, uniforms)
+    drawBufferInfo(
+      gl,
+      vertexArrayInfo,
+      gl.TRIANGLES,
+      vertexArrayInfo.numelements,
+      0,
+      this.particles,
+    )
   }
 }
--- src/webgl/scenes/particle.vs.glsl
+++ src/webgl/scenes/particle.vs.glsl
@@ -1,12 +1,16 @@
 attribute vec4 position;
 attribute vec3 normal;
+attribute mat4 instanceWorld;
 
 varying vec3 frag_Normal;
 
-uniform mat4 worldViewProjection;
-uniform mat4 worldView;
+uniform mat4 viewProjection;
+uniform mat4 view;
 
 void main() {
+  mat4 worldView = view * instanceWorld;
+  mat4 worldViewProjection = viewProjection * instanceWorld;
+
   gl_Position = worldViewProjection * position;
   frag_Normal = (worldView * vec4(normal, 0.0)).xyz;
 }
